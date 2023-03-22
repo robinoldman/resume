3aab0bff0560614d3c2fbe326ee95f5a61816c57
robinoldman <robin.c.oldman@gmail.com>
2023-03-22 15:20:32 +0000

    add js for git hub api

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,6 +8,24 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/images/Gear.gif" alt="loading..." />
+            <img src="assets/images/Loading-bar.gif" alt="loading..." />
         </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
