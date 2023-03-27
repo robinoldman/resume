bc6f6d412b384f7137c986d43c7adf0af55a572c
robinoldman <robin.c.oldman@gmail.com>
2023-03-27 11:39:15 +0000

    modified the email form to send to email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_3p1kq4s", "template_fnqi4j7", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
